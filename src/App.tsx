d2605178e24f48811cc505748b02d928dca8514b
vane-x <100387855+vane-x@users.noreply.github.com>
2022-08-12 21:57:56 -0400

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,35 @@
+import * as React from "react";
+import './App.css';
+import NavBar from './components/Navbar/Navbar';
+import { Navigate, Route, Routes } from 'react-router-dom';
+import About from './components/About/About';
+import Home from './components/Home/Home';
+import Error404 from './components/Error/Error404';
+import ItemDetailContainer from './components/Containers/ItemListContainer/ItemDetailContainer';
+import { ContextCard } from './Context/CardContext';
+import CartView from './components/CartView/CartView';
+
+function App() {
+
+  const { totalUnits } = React.useContext(ContextCard);
+
+  return (
+
+    <div className="App">
+      <NavBar quantityProduct={totalUnits} />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/Home' element={<Navigate to={"/"} replace />} />
+        <Route path='/:id' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/detail:id' element={<ItemDetailContainer />} />
+        <Route path='/CartView' element={<CartView />} />
+        <Route path='/*' element={<Error404 />} />        
+      </Routes>
+      <footer className='App-footer'>
+        Footer
+      </footer>
+    </div>
+  );
+} 
+export default App;
---
+++ src/components/Containers/ItemListContainer/ItemDetailContainer.tsx
@@ -0,0 +1,29 @@
+import * as React from "react";
+import ItemDetail from "../../ItemDetail/ItemDetail";
+import { useParams } from 'react-router-dom';
+import serviceJson from '../../services/serviceJson';
+
+const ItemListContainer = () => {
+  const [product, setProduct]: any = React.useState<any>({});
+  const param = useParams();
+
+  const getData = () => {
+    serviceJson().then((resp: any) => {
+      const itemSelected = resp.find((item: any) => item.id.toString() === param.id)
+      setProduct(itemSelected);
+    });
+  }
+  React.useEffect(() => {
+    getData();
+  }, [])
+
+  return (
+    <>
+      <br />
+        <ItemDetail itemProduct={product} />
+      <br />
+    </>
+  )
+};
+
+export default ItemListContainer;
